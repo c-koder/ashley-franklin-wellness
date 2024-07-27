ff519221e16576c0f3f40b956f79c9491535722d
c-koder <chamodnugekotuwa@gmail.com>
2024-07-27 12:36:40 +0530

    badge-added

--- src/utils/images.js
+++ src/utils/images.js
@@ -3,8 +3,16 @@ import LogoImg from "../assets/images/logo.png";
 import LogoDarkImg from "../assets/images/logo_dark.png";
 
 import PsychologyTodayLogo from "../assets/images/psychology-today.png";
+import certification1 from "../assets/images/certification1.png";
 
 import ptsVerified from "../assets/icons/pts-verified.png";
 import ptsVerified2 from "../assets/images/pts-verified2.png";
 
-export { LogoImg, LogoDarkImg, PsychologyTodayLogo, ptsVerified, ptsVerified2 };
+export {
+  LogoImg,
+  LogoDarkImg,
+  PsychologyTodayLogo,
+  ptsVerified,
+  ptsVerified2,
+  certification1,
+};
